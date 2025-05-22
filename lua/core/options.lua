b9418cba0bdb1863799365d476311f87c98cf7bc
Alexander Ivanov <alexander.n.ivanov@yandex.ru>
2025-05-22 10:02:32 +0300

    Use system clipboard

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -2,6 +2,7 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 vim.opt.backspace = '2'
+vim.opt.cb = 'unnamedplus' -- use system clipboard
 vim.opt.showcmd = true
 vim.opt.laststatus = 2
 vim.opt.autowrite = true
